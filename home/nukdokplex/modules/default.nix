d51623593f4e32536c32fd00b041d2dd11751bab
nukdokplex <nukdokplex@nukdokplex.ru>
2024-07-15 17:46:24 +0500

    add nheko (the matrix client)

--- home/nukdokplex/modules/default.nix
+++ home/nukdokplex/modules/default.nix
@@ -4,11 +4,12 @@
     ./chromium.nix
     ./gaming.nix
     ./git.nix
+    ./nheko.nix
+    ./rofi.nix
     ./spicetify.nix
     ./syncthing.nix
     ./userdirs.nix
     ./vscode.nix
     ./zsh.nix
-    ./rofi.nix
   ];
 }
