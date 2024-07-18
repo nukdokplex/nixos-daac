8b37044ba8462ba91157f239acc2c42413d622c4
nukdokplex <nukdokplex@nukdokplex.ru>
2024-07-18 22:04:16 +0500

    virtualbox -> libvirt

--- nixos/ndp-desktop/modules/virtualisation.nix
+++ nixos/ndp-desktop/modules/virtualisation.nix
@@ -1,5 +1,19 @@
-{ ... }: {
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.extraGroups.vboxusers.members = [ "nukdokplex" ];
+{ pkgs, ... }: {
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          pkgs.OVMFFull.fd
+        ];
+      };
+    };
+  };
+  users.users.nukdokplex.extraGroups =  [ "libvirtd" ];
+
+  programs.virt-manager.enable = true;
 }
