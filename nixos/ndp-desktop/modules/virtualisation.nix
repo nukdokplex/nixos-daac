174335b7d0f3857fc0b7236ed038a605190b74d5
nukdokplex <nukdokplex@nukdokplex.ru>
2024-07-19 01:32:36 +0500

    back to virtualbox

--- nixos/ndp-desktop/modules/virtualisation.nix
+++ nixos/ndp-desktop/modules/virtualisation.nix
@@ -1,19 +1,23 @@
 { pkgs, ... }: {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          pkgs.OVMFFull.fd
-        ];
-      };
-    };
-  };
-  users.users.nukdokplex.extraGroups =  [ "libvirtd" ];
+  # virtualisation.libvirtd = {
+  #   enable = true;
+  #   qemu = {
+  #     package = pkgs.qemu_kvm;
+  #     runAsRoot = true;
+  #     swtpm.enable = true;
+  #     ovmf = {
+  #       enable = true;
+  #       packages = [
+  #         pkgs.OVMFFull.fd
+  #       ];
+  #     };
+  #   };
+  # };
+  # users.users.nukdokplex.extraGroups =  [ "libvirtd" ];
 
-  programs.virt-manager.enable = true;
+  # programs.virt-manager.enable = true;
+
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  users.extraGroups.vboxusers.members = [ "nukdokplex" ];
 }
